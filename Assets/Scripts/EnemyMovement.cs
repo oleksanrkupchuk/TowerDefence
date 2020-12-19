10d63fee9569ba1a32ffacec460bcb0f38f6798f
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2020-12-19 19:39:06 +0200

    Changes on scene
    Added a prefab enemy, created script EnemyMovement, changed the position a the Map

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private int speed;
+    private Transform enemyNextTransform;
+    private GameObject road;
+
+    void Start()
+    {
+        road = GameObject.FindGameObjectWithTag("Road");
+        enemyNextTransform = road.transform.GetChild(0).transform.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, enemyNextTransform.position, speed * Time.deltaTime);
+    }
+}
