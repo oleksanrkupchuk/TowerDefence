bedf4f1b7caf4211e987a9c67077cf08c674d131
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2020-12-20 19:23:46 +0200

    Added a wave of enemy. Added place for towers
    Added a wave of enemy. Added place for towers. Changed the size of the sprites, added script TowerPlacing

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float timeSpawn;
+    [SerializeField] private float timer;
+    [SerializeField] private int enemyAmountInWave;
+    [SerializeField] private int enemyNumberWaves;
+    [SerializeField] private int enemyAmountSpawn;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        EnemySpawn();
+    }
+
+    private void EnemySpawn()
+    {
+        if(enemyPrefab != null)
+        {
+            if (enemyAmountSpawn < enemyAmountInWave)
+            {
+                timer += Time.deltaTime;
+
+                if (timer > timeSpawn)
+                {
+                    Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+                    enemyAmountSpawn++;
+                    timer = 0f;
+                }
+            }
+        }
+
+        else
+        {
+            Debug.LogError("'enemyPrefab' return null");
+        }
+        
+    }
+}
