a8b3fd1251ac88893868411e48c657e2d0e12b8b
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2021-12-14 09:23:40 +0200

    Added destroy animation for bullets
    Added destroy animation for bullets

---
+++ Assets/Scripts/Tower/BezierPoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierPoint : MonoBehaviour
+{
+    private Bullet _bullet;
+    
+    public void SetBullet(Bullet bullet) {
+        _bullet = bullet;
+    }
+
+    private void Start() {
+        _bullet.DestroyBeizerPoint += DestroyPoint;
+    }
+
+    public void DestroyPoint() {
+        _bullet.StopCalculation();
+        Destroy(gameObject);
+    }
+}
